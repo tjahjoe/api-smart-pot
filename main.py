16b02a62b395ea7223fdcab4d3cc75e1e25a7355
tjahjoe <ntell634@gmail.com>
2025-04-13 07:54:07 +0700

    add: file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from controller import Controller
 from flask import Flask
 
+app = Flask(__name__)
+controller = Controller(app)
+
 if __name__ == '__main__':
-    app = Flask(__name__)
-    controller = Controller(app)
     controller.run()
