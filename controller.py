2ac9f35cebc0ac48b7937349b6762f6241879e4e
tjahjoe <ntell634@gmail.com>
2025-04-28 13:10:24 +0700

    add: tes

--- controller.py
+++ controller.py
@@ -10,56 +10,80 @@ class Controller:
         self.__db_model = Model()
         self.__cloudinary = CloudinaryHandler()
 
-        self._setup_routes()
+        self.__setup_routes()
 
-    def _setup_routes(self):
-        self._app.add_url_rule('/post/image', view_func=self._post_image, methods=['POST'])
-        self._app.add_url_rule('/get/image', view_func=self._get_image, methods=['GET'])
-        self._app.add_url_rule('/insert/data', view_func=self._insert_data, methods=['POST'])
-        self._app.add_url_rule('/find/data', view_func=self._find_data, methods=['GET'])
+    def __setup_routes(self):
+        self._app.add_url_rule('/insert/user', view_func=self._insert_user, methods=['POST'])
+        self._app.add_url_rule('/post/image/<id>', view_func=self._post_image, methods=['POST'])
+        self._app.add_url_rule('/get/image/<id>', view_func=self._get_image, methods=['GET'])
+        self._app.add_url_rule('/insert/data/<id>', view_func=self._insert_data, methods=['POST'])
+        self._app.add_url_rule('/find/data/<id>', view_func=self._find_data, methods=['GET'])
 
-    def _post_image(self):
-        try:
-            image_bytes = request.data
-
-            url = self.__cloudinary.upload_image(image_bytes)
-
-            return jsonify({"message": "Image processed and uploaded successfully!", "url": url}), 200
-
-        except Exception as e:
-            return str(e), 500
-
-    def _get_image(self):
-        try:
-            image_url = "https://res.cloudinary.com/dkozkdqen/image/upload/1"
-
-            response = requests.get(image_url)
-            
-            if response.status_code == 200:
-                return send_file(io.BytesIO(response.content), mimetype='image/jpeg')
-            else:
-                return jsonify({"message": "Image not found."}), 404
-        except Exception as e:
-            return str(e), 500
-
-
-    def _insert_data(self):
+    def _insert_user(self):
         try:
             data = request.get_json()
-            ph = data.get('ph')
-            soil = data.get('soil')
-            self.__db_model.insert_data(ph, soil)
+            chat_id = data.get('chat_id')
+            pot_id = data.get('pot_id')
+            self.__db_model.insert_user(chat_id, pot_id)
+            self.__cloudinary.upload_image('white.jpg', public_id=str(pot_id))
 
-            return jsonify({'message': 'Data saved successfully'}), 201
+            return jsonify({'message': 'User saved successfully'}), 201
         except Exception as e:
             return str(e), 500
 
-    def _find_data(self):
+    def _post_image(self, id):
         try:
-            data = self.__db_model.find_data()
-            return jsonify(data), 200
+            id = int(id)
+            if self.__db_model.is_user(id):
+                image_bytes = request.data
+                url = self.__cloudinary.upload_image(image_bytes, public_id=str(id))
+                return jsonify({"message": "Image processed and uploaded successfully!", "url": url}), 200
+            else:
+                return jsonify({"message": "User not found."}), 404
         except Exception as e:
             return str(e), 500
 
+    def _get_image(self, id):
+        try:
+            id = int(id)
+            if self.__db_model.is_user(id):
+                image_url = self.__db_model.find_image(id)
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    return send_file(io.BytesIO(response.content), mimetype='image/jpeg')
+                else:
+                    return jsonify({"message": "Image not found."}), 404
+            else:
+                return jsonify({"message": f"User not found.{self.__db_model.is_user(id)}"}), 404
+        except Exception as e:
+            return str(e), 500
+
+
+    def _insert_data(self, id):
+        try:
+            id = int(id)
+            if self.__db_model.is_user(id):
+                data = request.get_json()
+                ph = data.get('ph')
+                soil = data.get('soil')
+                self.__db_model.insert_data(ph, soil)
+
+                return jsonify({'message': 'Data saved successfully'}), 201
+            else:
+                return jsonify({"message": "User not found."}), 404
+        except Exception as e:
+            return str(e), 500
+
+    def _find_data(self, id):
+        try:
+            id = int(id)
+            if self.__db_model.is_user():
+                data = self.__db_model.find_data()
+                return jsonify(data), 200
+            else:
+                return jsonify({"message": "User not found."}), 404
+        except Exception as e:
+            return str(e), 500
+        
     def run(self):
         self._app.run(host='0.0.0.0')
--- model.py
+++ model.py
@@ -6,13 +6,57 @@ import os
 
 class Model():
     def __init__(self):
-        self.__client = MongoClient(os.environ.get("MONGODB_API"),  server_api=ServerApi('1'))
+        # self.__client = MongoClient(os.environ.get("MONGODB_API"),  server_api=ServerApi('1'))
+        self.__client = MongoClient('mongodb+srv://elchilz:Elco1001@smartfarm.zrqtr.mongodb.net/?retryWrites=true&w=majority&appName=smartfarm',  server_api=ServerApi('1'))
+        self.__url = 'https://res.cloudinary.com/dkozkdqen/image/upload/'
         try:
             self.__database = self.__client.get_database('smartfarm')
-            self.__collection = self.__database.get_collection('data')
+            self.__collection()
+            self.__specification()
         except Exception as e:
             print(f'Error: {e}')
 
+    def __collection(self):
+        self.__collection_data = self.__database.get_collection('data')
+        self.__collection_user = self.__database.get_collection('user')
+        self.__collection_image = self.__database.get_collection('image')
+
+    def __specification(self):
+        self.__collection_user.create_index([('pot_id')], unique=True)
+
+    def insert_user(self, chat_id, pot_id):
+        user = {
+            'chat_id' : chat_id,
+            'pot_id' : pot_id
+            }
+
+        self.__collection_user.insert_one(user)
+        self.__insert_image(pot_id)
+
+
+    def is_user(self, pot_id):
+        user = self.__collection_user.find_one({'pot_id' : pot_id}, {'_id': 0,'chat_id' : 1}) 
+        if user == None:
+            return False
+        else:
+            return True       
+
+    def __insert_image(self, pot_id):
+        image = {
+            'pot_id' : pot_id,
+            'url' : f'{self.__url}{pot_id}'
+        }
+
+        self.__collection_image.insert_one(image)
+
+
+    def find_image(self, pot_id):
+        image = self.__collection_image.find_one({'pot_id' : pot_id}, {'_id': 0,'url' : 1})
+
+        if image == None:
+            return False
+        else:
+            return image['url']
 
     def insert_data(self, ph, soil):
         now = datetime.now()
@@ -34,10 +78,10 @@ class Model():
                 }
             }
         
-        self.__collection.insert_one(data)
+        self.__collection_data.insert_one(data)
     
     def find_data(self):
-        cursor = self.__collection.find({}, {'_id': 0, 'data.ph': 1, 'data.soil': 1}).sort('_id', -1).limit(10)
+        cursor = self.__collection_data.find({}, {'_id': 0, 'data.ph': 1, 'data.soil': 1}).sort('_id', -1).limit(10)
         raw_data = list(cursor)
 
         df = pd.json_normalize(raw_data) 
